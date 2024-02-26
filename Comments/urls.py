4d5979933bfec6efda9381a5239c4ed154e16358
danibarchen <danibarchene@gmail.com>
2024-02-26 10:54:28 +0200

    new edit

--- Comments/urls.py
+++ Comments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CommentsView
+from .views import CommentsView,PredictView
 
 urlpatterns = [
-    path('',CommentsView.as_view())
+    path('',CommentsView.as_view()),
+    path('com/',PredictView.as_view())
 ]
--- Comments/views.py
+++ Comments/views.py
@@ -8,3 +8,29 @@ from .serializers import CommentSerializers
 class CommentsView(APIView):
     permission_classes = [permissions.AllowAny,]
     serializer_class = CommentSerializers
+    
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import tensorflow as tf
+from tensorflow import keras
+import os
+from tensorflow.keras.layers import TextVectorization
+import numpy as np
+
+vectorizer = TextVectorization(max_tokens=200000,
+                               output_sequence_length=1800,
+                               output_mode='int')
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'false'
+class PredictView(APIView):
+    def post(self, request):
+        data = request.data
+        input_data = data['input_data']
+        input_data = vectorizer(input_data)
+        # input_data = tf.strings.unicode_decode(input_data, 'UTF-8')
+        model = keras.models.load_model('Comments/toxicity.h5')
+        prediction = model.predict(np.expand_dims(input_data,0))
+        return Response({'prediction': prediction})
+
+
