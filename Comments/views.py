5724ec410c55eb601c620626e9391bb21b8709e2
danibarchen <danibarchene@gmail.com>
2024-02-28 11:29:04 +0200

    add comments toxic model

--- Comments/views.py
+++ Comments/views.py
@@ -17,20 +17,32 @@ from tensorflow import keras
 import os
 from tensorflow.keras.layers import TextVectorization
 import numpy as np
+import pandas as pd
+from rest_framework.permissions import AllowAny
 
-vectorizer = TextVectorization(max_tokens=200000,
-                               output_sequence_length=1800,
-                               output_mode='int')
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'false'
 class PredictView(APIView):
+    permission_classes = (AllowAny,)
     def post(self, request):
         data = request.data
         input_data = data['input_data']
+        df = pd.read_csv(os.path.join('Comments','train.csv','train.csv'))
+        X = df['comment_text']
+        vectorizer = TextVectorization(max_tokens=200000,
+                               output_sequence_length=1800,
+                               output_mode='int')
+        vectorizer.adapt(X.values)
         input_data = vectorizer(input_data)
         # input_data = tf.strings.unicode_decode(input_data, 'UTF-8')
         model = keras.models.load_model('Comments/toxicity.h5')
         prediction = model.predict(np.expand_dims(input_data,0))
-        return Response({'prediction': prediction})
+        good =True
+        for idx, col in enumerate(df.columns[2:]):
+            if prediction[0][idx]>0.1:
+                good = False
+            else:
+                good = True    
+        return Response({'prediction': good})
 
 
