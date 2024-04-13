a5571e748b7c784c9c40adb5953d27bf6972e1f6
danibarchen7 <98905676+danibarchen7@users.noreply.github.com>
2024-04-13 22:47:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,14 +9,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations  Customer  
 python manage.py migrate Customer 
+python manage.py makemigrations Property  
+python manage.py migrate Property
 python manage.py makemigrations chat
 python manage.py migrate chat
 python manage.py makemigrations Favorites 
 python manage.py migrate Favorites 
 python manage.py makemigrations Images
 python manage.py migrate Images
-python manage.py makemigrations Property  
-python manage.py migrate Property
 python manage.py makemigrations Type
 python manage.py migrate Property  Type
 # Apply any outstanding database migrations
