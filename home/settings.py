6cea3361874583def38de1fd39fde320caf9b3d4
danibarchen7 <98905676+danibarchen7@users.noreply.github.com>
2024-04-14 12:15:31 +0300

    Update settings.py

--- home/settings.py
+++ home/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG","False").lower()=="true"
 DEBUG = False
-# ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'Customer.Customers'
 
